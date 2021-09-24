6773ca9aaf906c070c2b3d36f85062495167323f
Mahesh Ghamand <maheshghamand@gmail.com>
2021-09-24 12:41:38 +0530

    added more images

--- pages/images.js
+++ pages/images.js
@@ -1,7 +1,17 @@
 import Head from 'next/head'
 
 const imagePaths = {
-	'anime': ['boy_2.jpg', 'boy_3.png', 'chota-bheem_1.png']
+	'wallpaper': ['game_of_thrones_1.jpg', 'mansion_2.jpg'],
+	'anime': ['boy_2.jpg', 'boy_3.png', 'chota-bheem_1.png', 'boy_5.jpg'],
+	'dog': ['dog_1.jpg', 'dog_3.jpg'],
+	'logo': [
+		'solid8.png', 'blogger.svg', 'dev.to.svg', 'github.svg',
+		'gmail.svg', 'json.svg', 'linkedin.svg', 'mail.svg',
+		'telegram.svg', 'twitter.svg', 'vercel.svg',
+		'firebase-realtime-database.svg', 'firebase-auth.svg'
+	],
+	'quote': ['quote_1.png'],
+	'people': ['buddha_1.jpg', 'chhatrapati_3.jpg', 'darwin_1.jpg', 'elon_1.png']
 }
 
 const ImagesPage = () => {
@@ -12,18 +22,23 @@ const ImagesPage = () => {
 				<meta name="description" content="Images hosted for personal use" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<section className='d-flex flex-wrap'>
-				{
-					imagePaths.anime.map((path, idx) =>
-						<div key={idx} className="card" style={{ width: '300px' }}>
-							<img src={`/images/anime/${path}`} className="card-img-top" alt="..." />
-							<div className="card-body">
-								<a href={`/images/anime/${path}`} className="btn btn-primary">Direct Link</a>
-							</div>
-						</div>
-					)
-				}
-			</section>
+
+			{
+				Object.keys(imagePaths).map((folder, idx) => (
+					<section key={idx} className='d-flex flex-wrap'>
+						{
+							imagePaths[folder].map((path, idx) =>
+								<div key={idx} className="card" style={{ width: '100px' }}>
+									<img src={`/images/${folder}/${path}`} className="card-img-top" alt="..." />
+									<div className="card-body">
+										<a href={`/images/${folder}/${path}`} className="btn btn-primary">Link</a>
+									</div>
+								</div>
+							)
+						}
+					</section>
+				))
+			}
 		</div>
 	)
 }
